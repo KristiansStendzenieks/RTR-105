82e58379d8f116766c2e5a798b1a9805aa344527
kristiansstendzenieks <kristians.stendzenieks@edu.rtu.lv>
2022-09-29 00:32:47 +0300

    updated w/ tests

---
+++ tests.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+	int variableInt;
+	char variableChar;
+	double variableDouble;
+	
+	printf("display an int here:%d/n", variableInt);
+	printf("display a char here: %c/n", variableChar);
+	printf("display a double with 2 decimal places here: %.2f/n", variableDouble);
+
+}
