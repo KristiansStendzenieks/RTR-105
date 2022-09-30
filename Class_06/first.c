ad55e81dd49bdc22e464e49c270fc383fae11d14
kristiansstendzenieks <kristians.stendzenieks@edu.rtu.lv>
2022-09-30 11:33:35 +0300

    kristiansstendzenieks

---
+++ Class_06/first.c
@@ -0,0 +1,19 @@
+// komentāra viena rinda - // tiek apzīmēts komentāra sākums
+// komentāra cita rinda
+
+/*
+Komentāra paragrāfs
+Izmanto, piemēram, lai komentetu koda fragmentus
+*/
+
+int main()
+{
+//if()
+ //{
+ //
+ //}
+
+return 50;
+
+//return 69;
+}
